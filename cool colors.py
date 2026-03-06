5fb0d470fd6d8e28a3c0fb8a7b81d28cea713467
Mr1Skeletoner <glitchtrapissus@gmail.com>
2026-03-06 20:32:51 +0200

    Add files via upload

---
+++ cool colors.py
@@ -0,0 +1,59 @@
+print("\033[32m-- Code is running --\033[0m")
+print()
+
+# Source - https://stackoverflow.com/a/72970140
+# Posted by Shane S
+
+print("\033[31mThis is red font.\033[0m")
+print("\033[32mThis is green font.\033[0m")
+print("\033[33mThis is yellow font.\033[0m")
+print("\033[34mThis is blue font.\033[0m")
+print("\033[37mThis is the default font.\033[0m")
+
+
+print()
+
+print("\033[41m Highlighted Red Text\033[0m")
+print("\033[42m Highlighted Green Text\033[0m")
+print("\033[43m Highlighted Yellow Text\033[0m")
+print("\033[44m Highlighted Blue Text\033[0m")
+print("\033[45m Highlighted Magenta Text\033[0m")
+print("\033[46m Highlighted Cyan Text\033[0m")
+print("\033[47m Highlighted Gray Text\033[0m")
+
+# highlighiting on colored
+
+print("\033[31;43m Red text on a Highlighted Yellow background \033[0m")
+
+
+# another version:
+
+# Source - https://stackoverflow.com/a/78017374
+# Posted by Mildly Intelligent
+
+print("\033[00m00: nothing\033[0m")
+print("\033[01m01: bold?\033[0m")
+print("\033[02m02: gray\033[0m")
+print("\033[03m03: italic\033[0m")
+print("\033[04m04: underline\033[0m")
+print("\033[05m05-06: nothing\033[0m")
+print("\033[07m07: inverted\033[0m")
+print("\033[08m08: invisible?\033[0m")
+print("\033[09m09: strike through\033[0m")
+print("\033[10m10-20: nothing\033[0m")
+print("\033[21m21: double underline\033[0m")
+print("\033[22m22-29: nothing\033[0m")
+print("\033[30m30: gray\033[0m")
+print("\033[31m31: red\033[0m")
+print("\033[32m32: green\033[0m")
+print("\033[33m33: yellow\033[0m")
+print("\033[34m34: blue\033[0m")
+print("\033[35m35: magenta\033[0m")
+print("\033[36m36: cyan\033[0m")
+print("\033[37m37+: nothing\033[0m")
+
+
+
+
+print()
+print("\033[32m-- Code end --\033[0m")
